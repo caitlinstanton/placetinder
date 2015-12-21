e9e3b4d89a2711b2d43e5e0f865ffdf9ed23fbf8
igerstein <isaac.gerstein1@gmail.com>
2015-12-21 11:29:11 -0500

    Created routes and templates for each page

--- app.py
+++ app.py
@@ -6,6 +6,26 @@ app = Flask(__name__)
 def home():
     return render_template("home.html");
 
+@app.route("/login")
+def login():
+    return render_template("login.html");
+
+@app.route("/create")
+def create():
+    return render_template("create.html");
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html");
+
+@app.route("/results")
+def results():
+    return render_template("results.html");
+
+@app.route("/list")
+def list():
+    return render_template("list.html");
+
 if __name__ == "__main__":
     app.debug = True
     app.run(host='0.0.0.0',port=8000)
