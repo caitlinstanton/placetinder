5ec9932e5bdbdbe19008572115dd938a6778e5ac
igerstein <isaac.gerstein1@gmail.com>
2016-01-05 11:27:57 -0500

    Added description for each function

--- query.py
+++ query.py
@@ -2,16 +2,36 @@ from pymongo import MongoClient
 import hashlib
 
 def encrypt(word):
+    """
+    Encrypts a word using the sha256 hash algorithm.
+    Args:
+        word (str): The word to be encrypted.
+    Returns:
+        str: The encrypted word.
+    """
     hashp = hashlib.sha256()
     hashp.update(word)
     return hashp.hexdigest()
 
 def addUser(username,password):
+    """
+    Adds a user to the database.
+    Args:
+        username (str): The user's username.
+        password (str): The user's password.
+    """
     connection = MongoClient()
     db = connection['data']
     db.users.insert({'name':username, 'password':encrypt(password)})
 
 def userExists(username):
+    """
+    Checks whether a user exists in the database.
+    Args:
+        username (str): The username to be checked.
+    Returns:
+        boolean: Whether the user exists.
+    """
     connection = MongoClient()
     db = connection['data']
     res = db.users.find({'name':username})
@@ -20,6 +40,14 @@ def userExists(username):
     return False
 
 def authenticate(username, password):
+    """
+    Checks whether a username and password combination exists in the database.
+    Args:
+        username (str): The username to be checked.
+        password (str): The password to be checked.
+    Returns:
+        boolean: Whether the username and password combination exists.
+    """
     connection = MongoClient()
     db = connection['data']
     res = db.users.find({'name':username})
