6241e3a9a16cc2361780985d25847decb581ddb0
igerstein <isaac.gerstein1@gmail.com>
2016-01-07 11:44:46 -0500

    Work on StubHub API

--- stubhubapi.py
+++ stubhubapi.py
@@ -1,2 +1,8 @@
-# Example API request
-# https://api.stubhub.com/search/catalog/events/v3?status=active&state=NY
+import urllib2, json, base64
+
+url = "https://api.stubhub.com/search/catalog/events/v3?status=active&state=NY"
+headers = {"Authorization":"Bearer VsLaqXIQj3mygZTo36tu5rcoxkEa"}
+request = urllib2.Request(url, headers)
+result = urllib2.urlopen(request).read()
+r = json.loads(result)
+print r
