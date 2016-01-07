9d040abd4d04633ac8b3a8b62881a27720f2c42d
igerstein <isaac.gerstein1@gmail.com>
2016-01-07 11:45:28 -0500

    Update stubhubapi.py

--- stubhubapi.py
+++ stubhubapi.py
@@ -1,7 +1,7 @@
 import urllib2, json, base64
 
 url = "https://api.stubhub.com/search/catalog/events/v3?status=active&state=NY"
-headers = {"Authorization":"Bearer VsLaqXIQj3mygZTo36tu5rcoxkEa"}
+headers = {"Authorization":"Bearer "}
 request = urllib2.Request(url, headers)
 result = urllib2.urlopen(request).read()
 r = json.loads(result)
