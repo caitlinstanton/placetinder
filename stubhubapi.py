9aceeb656a45fa8829ab8fb169bbe723f8fdad0c
igerstein <isaac.gerstein1@gmail.com>
2016-01-12 19:46:43 -0500

    Added function to find StubHub events that match a search query

--- stubhubapi.py
+++ stubhubapi.py
@@ -1,10 +1,23 @@
 import urllib2, json
 
-url = "https://api.stubhub.com/search/catalog/events/v3"
-headers = {"Authorization":"Bearer VsLaqXIQj3mygZTo36tu5rcoxkEa",
-           "Accept":"application/json",
-           "Accept-Encoding":"application/json"}
-request = urllib2.Request(url, None, headers)
-result = urllib2.urlopen(request).read()
-r = json.loads(result)
-print r
+API_URL = "https://api.stubhub.com/search/catalog/events/v3"
+APPLICATION_TOKEN = "VsLaqXIQj3mygZTo36tu5rcoxkEa"
+
+def search(query):
+    """
+    Search the StubHub API for all events that match a search query.
+    Args:
+        query (str): The search query.
+    Returns:
+        list: A list of the JSON responses from all events that match the search query.
+    Raises:
+        urllib2.HTTPError: An error occurs from the HTTP request.
+    """
+    url = API_URL + "?q=" + query.replace(" ", "+")
+    headers = {"Authorization":"Bearer " + APPLICATION_TOKEN,
+               "Accept":"application/json",
+               "Accept-Encoding":"application/json"}
+    request = urllib2.Request(url, None, headers)
+    result = urllib2.urlopen(request).read()
+    r = json.loads(result)
+    return r["events"]
