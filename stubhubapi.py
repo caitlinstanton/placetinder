fcae7171c037d3de532bbfdde5936763be476ff3
igerstein <isaac.gerstein1@gmail.com>
2016-01-12 19:19:35 -0500

    Fixed another authorization problem

--- stubhubapi.py
+++ stubhubapi.py
@@ -1,7 +1,10 @@
-import urllib2
+import urllib2, json
 
-url = "https://api.stubhub.com/search/catalog/events/v3?status=active&state=NY"
-headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36"}
+url = "https://api.stubhub.com/search/catalog/events/v3"
+headers = {"Authorization":"Bearer VsLaqXIQj3mygZTo36tu5rcoxkEa",
+           "Accept":"application/json",
+           "Accept-Encoding":"application/json"}
 request = urllib2.Request(url, None, headers)
 result = urllib2.urlopen(request).read()
-print result
+r = json.loads(result)
+print r
