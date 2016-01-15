eda20b02a4ed0fade89c56f6b0512b9e445cb51d
igerstein <isaac.gerstein1@gmail.com>
2016-01-15 16:18:03 -0500

    Updated searchQuery function and description of search function

--- stubhubapi.py
+++ stubhubapi.py
@@ -3,17 +3,19 @@ import urllib2, json
 API_URL = "https://api.stubhub.com/search/catalog/events/v3"
 APPLICATION_TOKEN = "VsLaqXIQj3mygZTo36tu5rcoxkEa"
 
-def searchQuery(query):
+def searchQuery(query, num):
     """
-    Search the StubHub API for all events that match a search query.
+    Search the StubHub API for a number of events that match a search query.
     Args:
         query (str): The search query.
+        num (int): The number of events to return (maximum 500).
     Returns:
         list: A list of the JSON responses from all events that match the search query.
     Raises:
         urllib2.HTTPError: An error occurs from the HTTP request.
     """
-    url = API_URL + "?q=" + query.replace(" ", "+")
+    url = API_URL + "?rows=" + str(num)
+    url += "&q=" + query.replace(" ", "+")
     headers = {"Authorization":"Bearer " + APPLICATION_TOKEN,
                "Accept":"application/json",
                "Accept-Encoding":"application/json"}
@@ -24,13 +26,20 @@ def searchQuery(query):
 
 def search(eventType, coordinates, radius, price, earliestTime, latestTime):
     """
-    Search the StubHub API for all events that match the given parameters.
+    Search the StubHub API for the top 500 events that match the given parameters.
     Args:
-        eventType (str): The type of event (Concerts, Sports, Theater, etc.).
+        eventType (str): The type of event (Concert, Sports, Theater, etc.).
         coordinates (str): The latitude and longitude, separated by a comma.
         radius (float): The radius around the coordinates to search within.
         price (float): The approximate price in dollars.
-        earliestTime (str): The earliest possible time and date.
-        latestTime (str): The latest possible time and date.
+        earliestTime (str): The earliest possible time and date, in the form yyyy-mm-dd;hh:mm:ss (24-hour clock).
+        latestTime (str): The latest possible time and date, in the form yyyy-mm-dd;hh:mm:ss (24-hour clock).
     """
-    
+    url = API_URL + "?rows=500"
+    headers = {"Authorization":"Bearer " + APPLICATION_TOKEN,
+               "Accept":"application/json",
+               "Accept-Encoding":"application/json"}
+    request = urllib2.Request(url, None, headers)
+    result = urllib2.urlopen(request).read()
+    r = json.loads(result)
+    return r["events"]
