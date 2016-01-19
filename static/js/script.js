9a3a684b16449d935177b3873f059dc68f67526d
Vivian Li <vivian2938@gmail.com>
2016-01-19 06:55:54 -0500

    settings

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,3 @@
-//Button script from https://www.freshdesignweb.com/css3-buttons/
-$(".button-fill").hover(function () {
-    $(this).children(".button-inside").addClass('full');
-}, function() {
-    $(this).children(".button-inside").removeClass('full');
+$(function () {
+    $('#datetimepicker1').datetimepicker();
 });
