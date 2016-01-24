bcfc481be76fa3dd109d5b94b0143b87df124862
Caitlin Stanton <caitlinstanton44@gmail.com>
2016-01-23 19:44:02 -0500

    cleaning house

--- eventbrite/eventbrite4.py
+++
@@ -1,9 +0,0 @@
-import requests
-response = requests.get(
-    "https://www.evbdev.com/ebapi/v3/users/me/owned_events/",
-    headers = {
-        "Authorization": "Bearer JYHSYPNUFM546GH2YJSO",
-    },
-    verify = True,  # Verify SSL certificate
-)
-print response.json()['events'][0]['name']['text']
